673916bde374b878b3797655bcefc0edc04de6da
Rafay Malik <139852390+Rafay2216@users.noreply.github.com>
2023-12-24 16:49:18 +0500

    added further information

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -5,7 +5,7 @@ function Todolist() {
 
 
 
-    const [Task, setTask] = useState("");
+    const [Task, setTask] = useState(["study","sleep","eat","race"]);
     const [newTask, setnewTask] = useState("");
 
 
@@ -18,6 +18,12 @@ function Todolist() {
 
 
     function addtask(){
+if(newTask !==""){
+      setTask(task=>[...task, newTask])
+}
+else{
+  alert("add task bro")
+}
 
     }
 
@@ -25,14 +31,7 @@ function Todolist() {
 
     }
 
-    function  uptask(index){
-
-
-    }
-
-
-    function  downtask(index){
-
+    function  completedtask(index){
 
 
     }
@@ -40,15 +39,38 @@ function Todolist() {
   return (
    
     <>
-    
+     <div className='main'>
     <h1>Welcome to My To-Do-List App</h1>
 
     <div><input type="text"
     placeholder='add task to be added' 
     value={newTask}
     onChange={handleInputchange}/>
-    <button className='addbtn'></button>
+    <button 
+    className='addbtn'
+    onClick={addtask}>
+    add task
+    </button>
     </div>
+
+    <ol>
+      {Task.map((Task,index)=>
+<li key={index}>
+  <span className='text'>{Task}</span>
+  <button className='del' 
+  onClick={()=>deletetask(index)}>DELETE</button>
+
+<button className='comp' 
+  onClick={()=>completedtask(index)}>Completed</button>
+
+
+</li>
+
+      
+    )}
+    </ol>
+
+   </div>
     </>
   )
 }
